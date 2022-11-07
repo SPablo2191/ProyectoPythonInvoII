7acdd8c6cc030aa99b39a632ba93f0916d6ffc1d
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-07 16:29:29 -0300

    fix: carpetas de juego y redes

--- Redes/MST/MST.py
+++ Redes/MST/MST.py
@@ -1,41 +1,7 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 import random
-
-
-def ingresarGrafoSinPesos():
-    vertices: list = []
-
-    respuesta = "si"
-    print("Cada numero representa el numero de arco")
-    print("Ingreso de los arcos del grafo (0 es el inicial):")
-    while(respuesta == "si"):
-        nodoInicial = int(input("\tIngrese el primer nodo del arco: "))
-        nodoFinal = int(input("\tIngrese el segundo nodo del arco: "))
-        vertices.append([nodoInicial, nodoFinal])
-        respuesta = input("¿Desea ingresar arcos? si/no: ")
-
-    return ig.Graph(vertices)
-
-def ingresarGrafoConPesos():
-    vertices: list = []
-    pesos = []
-
-    respuesta = "si"
-    print("Cada numero representa el numero de arco")
-    print("Ingreso de los arcos del grafo (0 es el inicial):")
-    while(respuesta == "si"):
-        nodoInicial = int(input("\tIngrese el primer nodo del arco: "))
-        nodoFinal = int(input("\tIngrese el segundo nodo del arco: "))
-        vertices.append([nodoInicial, nodoFinal])
-        peso = float(input("\tIngrese el peso del arco: "))
-        pesos.append(peso)
-        respuesta = input("¿Desea ingresar arcos? si/no: ")
-
-    grafo = ig.Graph(vertices)
-    grafo.es["peso"] = pesos
-
-    return grafo
+from Grafos import ingresarGrafoConPesos
 
 grafo = ingresarGrafoConPesos()
 
