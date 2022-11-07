b250ad2beb4c5d8e1d1c33f2fe65d8ef0a629018
Pablo Sandoval <pablosandoval2191@gmail.com>
2022-11-07 19:02:36 -0300

    add: proyecto

--- app/Redes/FlujoMaximo/FlujoMaximo.py
+++ app/Redes/FlujoMaximo/FlujoMaximo.py
@@ -1,6 +1,8 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 from Grafos import ingresarGrafoSinPesos
+
+
 def ingresarCapacidad(grafo):
     capacidades = []
     for i in range(grafo.vcount()+1):
@@ -8,8 +10,9 @@ def ingresarCapacidad(grafo):
         capacidades.append(capacidadInd)
     grafo.es["capacity"] = capacidades
 
-def main(grafo,fuente=0,sumidero=1):
-    if(fuente == sumidero):
+
+def main(grafo, fuente=0, sumidero=1):
+    if (fuente == sumidero):
         return
     else:
         flow = grafo.maxflow(3, 0, capacity=grafo.es["capacity"])
@@ -18,22 +21,21 @@ def main(grafo,fuente=0,sumidero=1):
             grafo,
             target=ax,
             layout="circle",
+            edge_label=grafo.es["capacity"],
             vertex_label=range(grafo.vcount()),
             vertex_color="lightblue"
         )
         plt.title(f"Flujo Maximo: {flow.value}")
         plt.show()
 
-
+print('--------', end='')
+print('Modelo de Redes: Algoritmo de Flujo Maximo', end='')
+print('--------')
 g = ingresarGrafoSinPesos(directed=True)
 ingresarCapacidad(g)
-fuente = int(input(f"ingrese nodo fuente entre 0 y {g.vcount()}: "))
-sumidero = int(input(f"ingrese nodo fuente entre 0 y {g.vcount()}: ")) 
-main(g,fuente,sumidero)
-
-# g = ig.Graph(
-#     6,
-#     [(3, 2), (3, 4), (2, 1), (4, 1), (4, 5), (1, 0), (5, 0)],
-#     directed=True
-# )
-# g.es["capacity"] = [7, 8, 1, 2, 3, 4, 5]
+# Ejemplo
+# g = ig.Graph(4,[(0, 1), (0, 3), (0, 2), (1, 2), (1, 4), (2, 4), (2, 3), (3, 4),(3,2)],directed=False)
+# g.es["capacity"] = [20, 10, 30,  40, 30  , 20, 10, 20]
+fuente = int(input(f"ingrese nodo fuente entre 0 y {g.vcount()-1}: "))
+sumidero = int(input(f"ingrese nodo fuente entre 0 y {g.vcount()-1}: "))
+main(g, fuente, sumidero)
